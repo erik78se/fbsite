fdc35b5fbfc17385d9fc18c18cd79aa9b23a8acd
Erik Lönroth <erik.lonroth@gmail.com>
2017-05-25 13:53:21 +0200

    small fixes

--- events/forms.py
+++ events/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 # Form for registering FaceBook events
 class FaceBookEventForm(forms.Form):
-    facebook_event_id = forms.CharField(label='Facebook Event ID (123123)', max_length=24)
+    facebook_event_id = forms.CharField(label="Event ID: ", max_length=24 )
